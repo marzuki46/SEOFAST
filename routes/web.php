ac1b908ca388939b20840a0797cbabd5467e7231
marzuki46 <ohmjuki@gmail.com>
2026-06-28 15:17:25 +0700

    Change bulk-generate URL to start-ai-batch to bypass WAF

--- routes/web.php
+++ routes/web.php
@@ -160,7 +160,7 @@ Route::middleware(['auth'])->group(function () {
             Route::get('/prapost', [\App\Http\Controllers\Admin\ContentController::class, 'prapost'])->name('prapost');
             Route::get('/drafts', [\App\Http\Controllers\Admin\ContentController::class, 'drafts'])->name('drafts');
             Route::post('/work-queue', [\App\Http\Controllers\Admin\ContentController::class, 'workQueue'])->name('work_queue');
-            Route::match(['get', 'post'], '/bulk-generate', [\App\Http\Controllers\Admin\ContentController::class, 'bulkGenerateAi'])->name('bulk_generate');
+            Route::match(['get', 'post'], '/start-ai-batch', [\App\Http\Controllers\Admin\ContentController::class, 'bulkGenerateAi'])->name('bulk_generate');
             Route::get('/create', [\App\Http\Controllers\Admin\ContentController::class, 'create'])->name('create');
             Route::post('/', [\App\Http\Controllers\Admin\ContentController::class, 'store'])->name('store');
             
